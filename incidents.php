4370ce9d980d53ba615b2b9e871bddaadefe9362
MoeBrowne <oliver.browne@uplyme.com>
2017-12-30 16:17:33 +0000

    Extracted the rendering of the incident list to a function

---
+++ incidents.php
@@ -0,0 +1,61 @@
+<?php
+
+$incidents = json_decode(file_get_contents(__DIR__ . '/incidents.json'));
+$incidents = (array)$incidents;
+
+usort($incidents, function ($a, $b) {
+    if ($a->timestamp == $b->timestamp) {
+        return 0;
+    }
+    return ($a->timestamp < $b->timestamp) ? 1 : -1;
+});
+
+// Remove any incidents without a position
+$incidents = array_filter($incidents, function ($incident) {
+    return !empty($incident->location->lat) && !empty($incident->location->lng);
+});
+
+
+function renderIncidents($offset, $count)
+{
+    global $incidents;
+
+    $prevDateGroup = null;
+    $incidentGroup = array_slice($incidents, $offset, $count);
+    $entries = [];
+
+    foreach ($incidentGroup as $incident) {
+        if (date('d M Y', $incident->timestamp) !== $prevDateGroup) {
+            $entries[] = '
+            <li class="timeline-item period">
+                <div class="timeline-info"></div>
+                <div class="timeline-marker"></div>
+                <div class="timeline-content">
+                    <h2 class="timeline-title">' . date('j F Y', $incident->timestamp) . '</h2>
+                </div>
+            </li>';
+        }
+
+        $classes = '';
+
+        $searchString = $incident->title . ' ' . $incident->description;
+        $classes .= strpos($searchString, 'false alarm') !== false ? 'false-alarm ' : '';
+        $classes .= preg_match('/small vehicle|vehicle fire|Fire Vehicle|RTC|Road Traffic Collision|Car Fire/i', $searchString) === 1 ? 'vehicle-fire ' : '';
+        $classes .= preg_match('/large vehicle|vehicle large|Lorry Fire/i', $searchString) === 1 ? 'vehicle-fire-large ' : '';
+        $classes .= preg_match('/locked in|Shut In|Lift Release|Release person/i', $searchString) === 1 ? 'locked-in ' : '';
+        $classes .= preg_match('/small animal|RSPCA|hamster/i', $searchString) === 1 ? 'small-animal ' : '';
+        $classes .= preg_match('/aircraft/i', $searchString) === 1 ? 'aircraft ' : '';
+
+        $entries[] = '<li class="timeline-item">
+            <div class="timeline-marker ' . $classes . '"></div>
+            <div class="timeline-content">
+                <h3 class="timeline-title">' . $incident->title . '</h3>
+
+                <p>' . $incident->description . '</p>
+            </div>
+        </li>';
+        $prevDateGroup = date('d M Y', $incident->timestamp);
+    }
+
+    return implode('', $entries);
+}
